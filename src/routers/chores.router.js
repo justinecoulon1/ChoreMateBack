defd1c4a100b6f4467eeaf6a8dfde3694ca41ba6
Justine Coulon <justinecoulon1@gmail.com>
2025-02-15 17:27:32 +0100

    added add assignee and remove assignee routes and implementations

--- src/routers/chores.router.js
+++ src/routers/chores.router.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import choreController from '../controllers/chores.controllers.js';
-import validateChore from '../middlewares/chores/validateChore.js';
+import validateChoreMiddleware from '../middlewares/chores/validateChore.middleware.js';
+import validateAssigneeMiddleware from '../middlewares/chores/validateAssignee.middleware.js';
 
 const choresRouter = express.Router();
 
 choresRouter.get('/:id', choreController.details);
-choresRouter.post('/add', validateChore, choreController.addPOST);
-choresRouter.post('/:id/complete', choreController.completePOST);
+choresRouter.post('/add', validateChoreMiddleware, choreController.addPost);
+choresRouter.post('/:id/complete', choreController.completePost);
+choresRouter.post('/:id/add_assignee/:assigneeId', validateAssigneeMiddleware, choreController.addAssignee);
+choresRouter.post('/:id/remove_assignee/:assigneeId', validateAssigneeMiddleware, choreController.removeAssignee);
 
 export default choresRouter;
