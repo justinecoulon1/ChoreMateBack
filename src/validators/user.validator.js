df98aafa6585d3bd5ee1c581807aa7125557d877
Justine Coulon <justinecoulon1@gmail.com>
2025-03-13 18:18:51 +0100

    added PasswordSchema

--- src/validators/user.validator.js
+++ src/validators/user.validator.js
@@ -1,4 +1,13 @@
 import { z } from "zod";
+
+const PasswordSchema = z.string()
+  .min(8, "Password must be at least 8 characters long")
+  .max(64, "Password must be at most 64 characters long")
+  .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+  .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+  .regex(/[0-9]/, "Password must contain at least one number")
+  .regex(/[^A-Za-z0-9]/, "Password must contain at least one special character");
+
 export const UserRegisterSchema = z.object({
         name: z.string()
             .min(1, {message: 'Name is required'})
